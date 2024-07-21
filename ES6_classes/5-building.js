93d5e0498532803b5641e376c4ea1536334e424f
AnthonyCointre <8732@holbertonstudents.com>
2024-07-21 15:02:14 +0200

    creation 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }

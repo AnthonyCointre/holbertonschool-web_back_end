1464bd6830cde959128dafc7179f36d3e0fdc4a1
AnthonyCointre <8732@holbertonstudents.com>
2024-07-23 18:16:37 +0200

    creation 8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,8 +4,8 @@ export default function cleanSet(set, startString) {
   }
 
   const result = [...set]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
     .join('-');
 
   return result;

d296a78795f4b5a83199c42fb64a83fccf50f855
AnthonyCointre <8732@holbertonstudents.com>
2024-07-23 17:44:50 +0200

    creation 8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [...set]
+    .filter(value => value.startsWith(startString))
+    .map(value => value.slice(startString.length))
+    .join('-');
+
+  return result;
+}
+
+export default cleanSet;

26fa05dfa7e824a91aff7e1f08001dc800175783
AnthonyCointre <8732@holbertonstudents.com>
2024-07-23 18:02:12 +0200

    creation 7-has_array_values.js

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,3 @@
-function hasValuesFromArray(set, array) {
+export default function hasValuesFromArray(set, array) {
   return array.every(value => set.has(value));
 }
-
-export default hasValuesFromArray;

b122ad2ab475c743f6e7b5a4afe3cbebcdcd3178
AnthonyCointre <8732@holbertonstudents.com>
2024-08-19 18:30:44 +0200

    create 0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Coroutine that generates a sequence of 10 random floating-point numbers
+    between 0 and 10.
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

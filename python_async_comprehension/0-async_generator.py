02daa0e953bb01b8f1c42f2a3c274edef26eb6ad
AnthonyCointre <8732@holbertonstudents.com>
2024-08-20 10:01:59 +0200

    modify 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ Write a coroutine called async_generator that takes no arguments.
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that generates a sequence of 10 random floating-point numbers
     between 0 and 10.
